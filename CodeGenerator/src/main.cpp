00b739cd908c321c0f5e249c9da95585da8d47f6
nguyendaiphuc151006 <nguyendaiphuc151006@gmail.com>
2023-03-19 20:03:11 +0700

    Tách hàm trong code main. Code đã kiểm tra.

--- CodeGenerator/src/main.cpp
+++ CodeGenerator/src/main.cpp
@@ -20,7 +20,13 @@
 ** =============================================== */
 
 
-//
+void init_fuzzySets_input1();
+void init_fuzzySets_input2();
+void init_fuzzySets_output();
+void init_antecedents();
+void init_consequents();
+void init_rules();
+void init_fuzzy();
 
 
 /* ==================================================
@@ -85,75 +91,20 @@ void setup()
 
   randomSeed(analogRead(0));
 
-  
-  for(int i=0; i<num_fuzzySets_input1; ++i)
-  {
-    float a = membershipFunctions_input1[i];
-    float b = membershipFunctions_input1[i+1];
-    float c = membershipFunctions_input1[i+1];
-    float d = membershipFunctions_input1[i+2];
-
-    fuzzySets_input1[i] = FuzzySet(a, b, c, d);
-    fuzzyInput1.addFuzzySet(&fuzzySets_input1[i]);
-  }
-
-  for(int i=0; i<num_fuzzySets_input2; ++i)
-  {
-    float a = membershipFunctions_input2[i];
-    float b = membershipFunctions_input2[i+1];
-    float c = membershipFunctions_input2[i+1];
-    float d = membershipFunctions_input2[i+2];
-
-    fuzzySets_input2[i] = FuzzySet(a, b, c, d);
-    fuzzyInput2.addFuzzySet(&fuzzySets_input2[i]);
-  }
-
-  for(int i=0; i<num_fuzzySets_output; ++i)
-  {
-    float a = membershipFunctions_output[i];
-    float b = membershipFunctions_output[i+1];
-    float c = membershipFunctions_output[i+1];
-    float d = membershipFunctions_output[i+2];
-
-    fuzzySets_output[i] = FuzzySet(a, b, c, d);
-    fuzzyOutput.addFuzzySet(&fuzzySets_output[i]);
-  }
-
-  for(int i=0; i<num_fuzzySets_input1; ++i){
-    for(int j=0; j<num_fuzzySets_input2; ++j)
-    {
-      antecedents[i*num_fuzzySets_input2+j].joinWithAND(&fuzzySets_input1[i], &fuzzySets_input2[j]);
-    }
-  }
-
-  for(int i=0; i<num_rules; ++i)
-  {
-    for(int j=0; j<num_fuzzySets_output; ++j)
-    {
-      if(consequents_value[i] == membershipFunctions_output[j+1]){
-        consequents[i].addOutput(&fuzzySets_output[j]);
-        break;
-      }
-    }
-
-    rules[i] = FuzzyRule(i+1, &antecedents[i], &consequents[i]);
-  }
-
-  fuzzy.addFuzzyInput (&fuzzyInput1);
-  fuzzy.addFuzzyInput (&fuzzyInput2);
-  fuzzy.addFuzzyOutput (&fuzzyOutput);
-
-  for(int i=0; i<num_rules; ++i){
-    fuzzy.addFuzzyRule(&rules[i]);
-  }
-
+  init_fuzzySets_input1();
+  init_fuzzySets_input2();
+  init_fuzzySets_output();
+  init_antecedents();
+  init_consequents();
+  init_rules();
+  init_fuzzy();
 }
 
 
 void loop()
 {
-float input1 = random(21, 43);
-float input2 = random(0, 100);
+float input1 = random(membershipFunctions_input1[1], membershipFunctions_input1[num_fuzzySets_input1]);
+float input2 = random(membershipFunctions_input2[1], membershipFunctions_input2[num_fuzzySets_input2]);
 
 fuzzy.setInput(INDEX_FUZZY_INPUT1, input1);
 fuzzy.setInput(INDEX_FUZZY_INPUT2, input2);
@@ -173,3 +124,92 @@ delay(2000);
 ** =============================================== */
 
 
+void init_fuzzySets_input1()
+{
+  for(int i=0; i<num_fuzzySets_input1; ++i)
+  {
+    float a = membershipFunctions_input1[i];
+    float b = membershipFunctions_input1[i+1];
+    float c = membershipFunctions_input1[i+1];
+    float d = membershipFunctions_input1[i+2];
+
+    fuzzySets_input1[i] = FuzzySet(a, b, c, d);
+    fuzzyInput1.addFuzzySet(&fuzzySets_input1[i]);
+  }
+}
+
+
+void init_fuzzySets_input2()
+{
+  for(int i=0; i<num_fuzzySets_input2; ++i)
+  {
+    float a = membershipFunctions_input2[i];
+    float b = membershipFunctions_input2[i+1];
+    float c = membershipFunctions_input2[i+1];
+    float d = membershipFunctions_input2[i+2];
+
+    fuzzySets_input2[i] = FuzzySet(a, b, c, d);
+    fuzzyInput2.addFuzzySet(&fuzzySets_input2[i]);
+  }
+}
+
+
+void init_fuzzySets_output()
+{
+  for(int i=0; i<num_fuzzySets_output; ++i)
+  {
+    float a = membershipFunctions_output[i];
+    float b = membershipFunctions_output[i+1];
+    float c = membershipFunctions_output[i+1];
+    float d = membershipFunctions_output[i+2];
+
+    fuzzySets_output[i] = FuzzySet(a, b, c, d);
+    fuzzyOutput.addFuzzySet(&fuzzySets_output[i]);
+  }
+}
+
+
+void init_antecedents()
+{
+  for(int i=0; i<num_fuzzySets_input1; ++i){
+    for(int j=0; j<num_fuzzySets_input2; ++j)
+    {
+      antecedents[i*num_fuzzySets_input2+j].joinWithAND(&fuzzySets_input1[i], &fuzzySets_input2[j]);
+    }
+  }
+}
+
+
+void init_consequents()
+{
+  for(int i=0; i<num_rules; ++i)
+  {
+    for(int j=0; j<num_fuzzySets_output; ++j)
+    {
+      if(consequents_value[i] == membershipFunctions_output[j+1]){
+        consequents[i].addOutput(&fuzzySets_output[j]);
+        break;
+      }
+    }
+  }
+}
+
+
+void init_rules()
+{
+  for(int i=0; i<num_rules; ++i){
+    rules[i] = FuzzyRule(i+1, &antecedents[i], &consequents[i]);
+  }
+}
+
+
+void init_fuzzy()
+{
+  fuzzy.addFuzzyInput  (&fuzzyInput1);
+  fuzzy.addFuzzyInput  (&fuzzyInput2);
+  fuzzy.addFuzzyOutput (&fuzzyOutput);
+
+  for(int i=0; i<num_rules; ++i){
+    fuzzy.addFuzzyRule(&rules[i]);
+  }
+}
